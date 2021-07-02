7598219a71ce4274fcc8825fe1a67465e94e5ca2
Alejandrom-GO <miguel.a.gutierrezortega@gmail.com>
2021-07-01 22:39:47 -0500

    preloader pages- queryloader

---
+++ js/queryloader.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+    QueryLoader2(document.querySelector("body"), {
+        barColor: "#111",
+        backgroundColor: "#ffff",
+        percentage: true,
+        barHeight: 0,
+        minimumTime: 300,
+        fadeOutTime: 100
+    });
+});
